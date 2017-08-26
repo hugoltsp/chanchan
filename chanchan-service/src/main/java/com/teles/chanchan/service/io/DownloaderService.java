d8a53922036c879600a5468ac6b22be5d0f60bb4
hugoltsp <hugo-pires@outlook.com>
2017-08-26 17:12:30 -0300

    improved IO, much less memory consumption when downloading content

--- chanchan-service/src/main/java/com/teles/chanchan/service/io/DownloaderService.java
+++ chanchan-service/src/main/java/com/teles/chanchan/service/io/DownloaderService.java
@@ -4,58 +4,63 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.teles.chanchan.domain.exception.ChanchanMediaDownloadException;
 import com.teles.chanchan.domain.settings.ChanchanSettings;
 
 @Service
 public class DownloaderService {
 
+	private static final int EOF = -1;
+
 	private static final Logger logger = LoggerFactory.getLogger(DownloaderService.class);
 
 	private static final int BUFFER_SIZE = 16384;
 
-	private final ChanchanSettings settings;
+	private final String outputPath;
 
 	public DownloaderService(ChanchanSettings settings) {
-		this.settings = settings;
+		this.outputPath = settings.getIo().getOutputPath();
 	}
 
-	public void downloadImage(String path) throws ChanchanMediaDownloadException {
+	public void downloadImage(String path) {
 
 		try {
+
 			logger.info("Downloading image at:: {}", path);
+
 			URL url = new URL(path);
-			byte[] bytes = IOUtils.toByteArray(url);
-			this.writeImage(url.getPath(), bytes);
-		} catch (IOException | ChanchanMediaDownloadException e) {
+			File file = createFile(url.getPath());
+
+			try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file), BUFFER_SIZE);
+					InputStream inputStream = url.openStream()) {
+				write(outputStream, inputStream);
+			}
+
+		} catch (IOException e) {
 			logger.error("Could not download file at :: {} ", path, e);
 		}
 	}
 
-	private void writeImage(String name, byte[] bytes) throws ChanchanMediaDownloadException {
-		OutputStream outputStream = null;
-
-		try {
-			String path = this.settings.getIo().getOutputPath() + name;
-			File file = new File(path);
-			file.getParentFile().mkdirs();
-
-			outputStream = new BufferedOutputStream(new FileOutputStream(file), BUFFER_SIZE);
-			outputStream.write(bytes);
-		} catch (IOException e) {
-			throw new ChanchanMediaDownloadException(e);
-		} finally {
-			IOUtils.closeQuietly(outputStream);
+	private static void write(OutputStream outputStream, InputStream inputStream) throws IOException {
+		byte[] buf = new byte[BUFFER_SIZE];
+		int n;
+		while (EOF != (n = inputStream.read(buf))) {
+			outputStream.write(buf, 0, n);
 		}
+	}
 
+	private File createFile(String name) {
+		String path = new StringBuilder().append(this.outputPath).append(name).toString();
+		File file = new File(path);
+		file.getParentFile().mkdirs();
+		return file;
 	}
 
 }
